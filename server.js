f66b247eaa734365ab4803163461000085e104bd
SpaceDandyA80 <58712760+SpaceDandyA80@users.noreply.github.com>
2020-08-08 14:33:08 -0700

    empty commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.Port || 3000;
 
 const app = express();
 
@@ -15,8 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://OnOff:SpaceDandy1@ds129651.mlab.com:29651/heroku_0pbtxc1r", {
+  useUnifiedTopology:true,
+ 
+useNewUrlParser: true,
   useFindAndModify: false
 });
 
